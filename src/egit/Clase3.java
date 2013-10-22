635d45aea7036169c76226dfe9a4162d1c1abafc
Sergio Esposito <Sergio Esposito@192.168.1.12>
2013-10-22 21:52:26 +0200

    CD10

--- src/egit/Clase3.java
+++ src/egit/Clase3.java
@@ -1,5 +1,19 @@
 package egit;
 
 public class Clase3 {
+private int valor;
+
+public Clase3(int valor) {
+	super();
+	this.valor = valor;
+}
+
+public int getValor() {
+	return valor;
+}
+
+public void setValor(int valor) {
+	this.valor = valor;
+}
 
 }
