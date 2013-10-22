847aa9b8b971e72f47b22b6d3cca147891d1f1b9
Sergio Esposito <Sergio Esposito@192.168.1.12>
2013-10-22 20:26:07 +0200

    CD3

--- src/egit/Clase1.java
+++ src/egit/Clase1.java
@@ -1,20 +1,29 @@
 package egit;
 
 public class Clase1 {
-   private int valor;
-   private String descripcion;
+	private int valor;
+	private String descripcion;
 
-public Clase1(int valor) {
-	super();
-	this.valor = valor;
-}
+	public Clase1(int valor, String descripcion) {
+		super();
+		this.valor = valor;
+		this.descripcion = descripcion;
+	}
 
-public int getValor() {
-	return valor;
-}
+	public int getValor() {
+		return valor;
+	}
 
-public void setValor(int valor) {
-	this.valor = valor;
-}
-   
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 }
