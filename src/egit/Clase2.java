bad50b3196857ca588b199323bbe70e7415c3faf
Sergio Esposito <Sergio Esposito@192.168.1.12>
2013-10-22 20:59:21 +0200

    CD8

--- src/egit/Clase2.java
+++ src/egit/Clase2.java
@@ -2,10 +2,12 @@ package egit;
 
 public class Clase2 {
 	private int valor;
+	private String descripcion;
 
-	public Clase2(int valor) {
+	public Clase2(int valor, String descripcion) {
 		super();
 		this.valor = valor;
+		this.descripcion = descripcion;
 	}
 
 	public int getValor() {
