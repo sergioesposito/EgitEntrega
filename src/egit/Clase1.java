11a620a934031365f6255d7b0774faf8d717faaf
Sergio Esposito <Sergio Esposito@192.168.1.12>
2013-10-22 20:22:46 +0200

    CD1

---
+++ src/egit/Clase1.java
@@ -0,0 +1,19 @@
+package egit;
+
+public class Clase1 {
+   private int valor;
+
+public Clase1(int valor) {
+	super();
+	this.valor = valor;
+}
+
+public int getValor() {
+	return valor;
+}
+
+public void setValor(int valor) {
+	this.valor = valor;
+}
+   
+}
